096e1079023263bff4352f68313f2ad0775a4816
pravinkumarosingh <pravinkumarosingh@gmail.com>
2024-01-21 11:46:04 +0530

    initial commit

---
+++ src/main/java/org/example/MultipleOf3And5.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class MultipleOf3And5 {
+}
---
+++ src/test/java/org/example/MultipleOf3And5Test.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import junit.framework.TestCase;
+
+public class MultipleOf3And5Test extends TestCase {
+
+}
